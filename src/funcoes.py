7fd4ca79903ee1f6475d43da9613d270869872e7
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 00:01:15 +0100

    Alterações 05

---
+++ src/funcoes.py
@@ -0,0 +1,22 @@
+def cond_fuel(cond, fuel):
+    df_fuel = df[df['fuel'] == fuel]
+    df_cond = df_fuel[df_fuel['condition'] == cond]
+
+    return df_cond['model']
+
+
+def dropar_dados_faltantes(df, coluna, limite_dados_faltantes):
+    if df[coluna].isna().mean() > limite_dados_faltantes:
+        df = df.dropna(subset=[coluna])
+
+    return df
+
+
+def limpar_outlier(df, coluna):
+    media = df[coluna].mean()  # calculo da média
+    dp = df[coluna].std()  # desvio padrão
+    ls = media + 2*dp  # limite superior
+    li = media - 2*dp  # limite inferior
+    df = df[(df[coluna] > li) & (df[coluna] < ls)]
+
+    return df
