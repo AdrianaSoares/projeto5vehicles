2625fb140ea7d30e8ee402433527fd24dd429bd2
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 00:35:18 +0100

    alteração 07

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,3 +1,5 @@
+# função para filtragem com base em um DF, condição e combustivel
+
 def cond_fuel(df, cond, fuel):
     df_fuel = df[df['fuel'] == fuel]
     df_cond = df_fuel[df_fuel['condition'] == cond]
@@ -5,12 +7,16 @@ def cond_fuel(df, cond, fuel):
     return df
 
 
+# função para remoção de colunas com dados faltantes escolhendo um limite
+
 def dropar_dados_faltantes(df, coluna, limite_dados_faltantes):
     if df[coluna].isna().mean() > limite_dados_faltantes:
         df = df.dropna(subset=[coluna])
 
     return df
 
+
+# função para limpeza outlier
 
 def outlier(df, coluna):
     media = df[coluna].mean()  # calculo da média
