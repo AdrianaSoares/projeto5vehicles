d14d02eb2fbd18afd6146ce12543b2996a5aeb80
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 23:55:59 +0100

    Alteração 19

--- src/app.py
+++ src/app.py
@@ -34,3 +34,10 @@ if hist_button:  # se o botão for clicado
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+
+# criar uma caixa de seleção
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram:  # se a caixa de seleção for selecionada
+    st.write('Criando um histograma para a coluna odometer')
