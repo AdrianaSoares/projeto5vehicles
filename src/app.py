ffcccffcfbb6dcedb40b9bcccb4d1d3650ded69a
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 23:31:25 +0100

    Alteração 17

--- src/app.py
+++ src/app.py
@@ -18,3 +18,19 @@ st.title("Análise de Vendas de Automóveis")
 fig = px.histogram(df, x='price', color='condition')
 
 st.plotly_chart(fig)
+
+# Criando um histograma para o conjunto de dados de anúncios de vendas de carros
+
+car_data = pd.read_csv('vehicles_us.csv')  # lendo os dados
+hist_button = st.button('Criar histograma')  # criar um botão
+
+if hist_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
