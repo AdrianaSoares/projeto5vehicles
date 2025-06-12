dc765eafc6e3d4b2895a9aaab75eb519f61aa49b
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 01:33:14 +0100

    alteração 11

--- src/app.py
+++ src/app.py
@@ -2,6 +2,6 @@ import pandas as pd
 from pathlib import Path
 import streamlit as st
 
-df = pd.read_csv(Path("../data/vehicles.csv"))
+df = pd.read_csv(Path("data/vehicles.csv"))
 
 st.title("Análise de Vendas de Automóveis")
