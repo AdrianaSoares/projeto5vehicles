0d9c1a7d58e0b6f370def3a6794bdd0f071cce67
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 01:41:17 +0100

    alteração 11

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from pathlib import Path
 import streamlit as st
+import plotly_express as px
 
 df = pd.read_csv(Path("data/vehicles.csv"))
 
 st.title("Análise de Vendas de Automóveis")
+
+fig = px.histogram(df, x='price', color='fuel')
+
+st.plotly_chart(fig)
