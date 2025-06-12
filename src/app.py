383d09dc994f057e7693687b8ea065466c824724
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-12 02:18:29 +0100

    alteração15

--- src/app.py
+++ src/app.py
@@ -5,9 +5,13 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 import streamlit as st
 import plotly_express as px
-
+from src import funcoes
 
 df = pd.read_csv(Path("data/vehicles.csv"))
+df = funcoes.dropar_dados_faltantes(df, 'price', 0.1)
+df = funcoes.cond_fuel(df, 'good', 'gas')
+df = funcoes.outlier(df, 'price')
+
 
 st.title("Análise de Vendas de Automóveis")
 
